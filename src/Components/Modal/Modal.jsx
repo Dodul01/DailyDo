a9a4e9f8b23cc7d33df10a48cf298514a5016a63
Dodul01 <mhdodul299@gmail.com>
2023-12-22 03:28:00 +0600

    Delete Functionlity added

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,28 +1,60 @@
+import { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { RxCross2 } from "react-icons/rx";
+import { AppContext } from "../../AppContext/AppContextProvider";
+import toast from "react-hot-toast";
 
 const Modal = ({ setIsClicked }) => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm()
+    const { currentUser } = useContext(AppContext);
+
+    const onSubmit = (data) => {
+        const email = currentUser.email;
+        const task = { ...data, email , status: ''}
+
+        fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.insertedId) {
+                    toast.success('Task Added Successfully.')
+                }
+            })
+        reset()
+    }
+
     return (
         <div className='absolute top-0 left-0 bg-black bg-opacity-5 h-screen w-screen transition-all'>
             <div className='flex items-center justify-center h-full'>
                 <div className='bg-white w-[600px] shadow rounded-xl'>
                     <button className="float-right m-4 text-2xl" onClick={() => setIsClicked(false)}><RxCross2 /></button>
                     <h1 className="text-lg font-semibold text-center my-2">Add Task</h1>
-                    <form className="flex flex-col p-5 w-full gap-2">
+                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-5 w-full gap-2">
                         <label className="text-lg">Task Title</label>
-                        <input className="border p-2 rounded-lg outline-none" type="text" placeholder="Task Title" />
+                        <input className="border p-2 rounded-lg outline-none" {...register("title", { required: true })} type="text" placeholder="Task Title" />
+                        {errors.title && <span className="text-red-500 text-sm">This field is required</span>}
                         <label className="text-lg">Task Description</label>
-                        <textarea className="border p-2 rounded-lg outline-none" cols="10" rows="2" placeholder="Task Description"></textarea>
+                        <textarea className="border p-2 rounded-lg outline-none" cols="10" rows="2"  {...register("description", { required: true })} placeholder="Task Description"></textarea>
+                        {errors.description && <span className="text-red-500 text-sm">This field is required</span>}
                         <label className="text-lg">To</label>
-                        <input className="border p-2 rounded-lg outline-none" type="date" placeholder="from" />
+                        <input className="border p-2 rounded-lg outline-none" {...register("form", { required: true })} type="date" placeholder="from" />
+                        {errors.form && <span className="text-red-500 text-sm">This field is required</span>}
                         <label className="text-lg">From</label>
-                        <input className="border p-2 rounded-lg outline-none" type="date" placeholder="to" />
+                        <input className="border p-2 rounded-lg outline-none" type="date" {...register("to", { required: true })} placeholder="to" />
+                        {errors.to && <span className="text-red-500 text-sm">This field is required</span>}
                         <label className="text-lg">Set Priority</label>
-                        <select className="border p-2 rounded-lg outline-none" name="priority">
+                        <select className="border p-2 rounded-lg outline-none" {...register("priority", { required: true })} name="priority">
                             <option value="null">Select Priority</option>
                             <option value="high">High</option>
                             <option value="moderate">Moderate</option>
                             <option value="low">Low</option>
                         </select>
+                        {errors.priority && <span className="text-red-500 text-sm">This field is required</span>}
                         <button className="bg-blue-600 py-2 text-white text-lg rounded-lg mt-4">Add Task</button>
                     </form>
                 </div>
--- src/Pages/Dashbord/Dashbord.jsx
+++ src/Pages/Dashbord/Dashbord.jsx
@@ -2,14 +2,36 @@ import { IoHome } from "react-icons/io5";
 import { LuLayoutGrid } from "react-icons/lu";
 import { RxAvatar } from "react-icons/rx";
 import { FaPowerOff } from "react-icons/fa";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../AppContext/AppContextProvider";
 import { AiFillPlusCircle } from "react-icons/ai";
 import Modal from "../../Components/Modal/Modal";
-
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
+import toast from "react-hot-toast";
 const Dashbord = () => {
   const { currentUser } = useContext(AppContext);
   const [isClicked, setIsClicked] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data.deletedCount > 0){
+        toast.success('Task delete successfully.')
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/tasks?email=${currentUser?.email}`)
+      .then((res) => res.json())
+      .then((data) => setTasks(data))
+  }, [currentUser, isClicked])
 
   return (
     <div className='flex max-w-screen-2xl mx-auto rounded border min-h-screen'>
@@ -54,8 +76,8 @@ const Dashbord = () => {
           </div>
 
           <div>
-            <button onClick={()=> setIsClicked(true)} className="bg-blue-600 text-white px-3 py-2 text-lg font-semibold flex items-center justify-center gap-1 rounded-lg"><AiFillPlusCircle className="text-2xl font-bold mt-1" /> Create Task</button>
-            {isClicked && <Modal setIsClicked={setIsClicked}/>}
+            <button onClick={() => setIsClicked(true)} className="bg-blue-600 text-white px-3 py-2 text-lg font-semibold flex items-center justify-center gap-1 rounded-lg"><AiFillPlusCircle className="text-2xl font-bold mt-1" /> Create Task</button>
+            {isClicked && <Modal setIsClicked={setIsClicked} />}
           </div>
         </div>
         {/* Name and Image */}
@@ -63,15 +85,36 @@ const Dashbord = () => {
         {/* task section */}
         <div className="flex items-center justify-between w-full">
           <div className="w-full p-2 h-[80vh] overflow-hidden">
-            <h1 className="text-lg font-semibold">On Going</h1>
-
+            <h1 className="text-lg font-semibold">Todo Task</h1>
+            {/* Todo Task */}
+            {tasks.map(task => {
+              return <div className="bg-blue-50 border p-2 rounded-lg m-2 w-[94%]" key={task._id}>
+                <div className="flex items-center justify-between gap-1 mb-3">
+                  <h2 className="text-lg font-semibold">{task?.title}</h2>
+                  <p className="bg-blue-600 text-white w-[100px] text-center rounded-full">{task.priority}</p>
+                </div>
+                <p className="text-gray-600">{task.description}</p>
+                <div className="flex items-center mt-3 gap-2">
+                  <FaRegCalendarAlt className="text-gray-700" />
+                  <p>Form: {task.form} - </p>
+                  <p>To: {task.to}</p>
+                </div>
+                <div className="flex items-center justify-end gap-4 mt-2">
+                  <FaPen className="text-lg text-gray-700 cursor-pointer" />
+                  <FaTrash onClick={()=> handleDelete(task._id)} className="text-lg text-gray-700 cursor-pointer" />
+                </div>
+              </div>
+            })}
           </div>
           <div className="w-full p-2 border-l h-[80vh] overflow-hidden">
-            <h1 className="text-lg font-semibold">Panding task</h1>
+            <h1 className="text-lg font-semibold">On Going task</h1>
+            {/* On Going Task */}
 
           </div>
           <div className="w-full p-2 border-l h-[80vh] overflow-hidden">
             <h1 className="text-lg font-semibold">Complited task</h1>
+            {/* Complited Task */}
+
           </div>
         </div>
         {/* Task Section */}
