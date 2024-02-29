3687f144cb3fec417ffaecf3bb3bfb95f468be3c
Dodul01 <mhdodul299@gmail.com>
2024-02-29 19:55:38 +0600

    add people to task list created

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -29,7 +29,7 @@ const Modal = ({ setIsClicked, children }) => {
     }
 
     return (
-        <div className='absolute top-0 left-0 bg-black bg-opacity-50 h-screen w-screen transition-all'>
+        <div className='absolute top-0 left-0 z-20 bg-black bg-opacity-40 h-screen w-screen transition-all'>
             <div className='flex items-center justify-center h-full'>
                 <div className='bg-white w-[600px] shadow rounded-xl'>
                     <button className="float-right m-4 text-2xl" onClick={() => setIsClicked(false)}><RxCross2 /></button>
@@ -38,31 +38,6 @@ const Modal = ({ setIsClicked, children }) => {
                         {children}
                     </div>
                     {/* Form Content Ends Here */}
-
-
-                    {/* <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-5 w-full gap-2">
-                        <label className="text-lg">Task Title</label>
-                        <input className="border p-2 rounded-lg outline-none" {...register("title", { required: true })} type="text" placeholder="Task Title" />
-                        {errors.title && <span className="text-red-500 text-sm">This field is required</span>}
-                        <label className="text-lg">Task Description</label>
-                        <textarea className="border p-2 rounded-lg outline-none" cols="10" rows="2"  {...register("description", { required: true })} placeholder="Task Description"></textarea>
-                        {errors.description && <span className="text-red-500 text-sm">This field is required</span>}
-                        <label className="text-lg">To</label>
-                        <input className="border p-2 rounded-lg outline-none" {...register("form", { required: true })} type="date" placeholder="from" />
-                        {errors.form && <span className="text-red-500 text-sm">This field is required</span>}
-                        <label className="text-lg">From</label>
-                        <input className="border p-2 rounded-lg outline-none" type="date" {...register("to", { required: true })} placeholder="to" />
-                        {errors.to && <span className="text-red-500 text-sm">This field is required</span>}
-                        <label className="text-lg">Set Priority</label>
-                        <select className="border p-2 rounded-lg outline-none" {...register("priority", { required: true })} name="priority">
-                            <option value="null">Select Priority</option>
-                            <option value="high">High</option>
-                            <option value="moderate">Moderate</option>
-                            <option value="low">Low</option>
-                        </select>
-                        {errors.priority && <span className="text-red-500 text-sm">This field is required</span>}
-                        <button className="bg-blue-600 py-2 text-white text-lg rounded-lg mt-4">Add Task</button>
-                    </form> */}
                 </div>
             </div>
         </div>
